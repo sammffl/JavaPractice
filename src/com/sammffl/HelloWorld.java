769303ee668ed0f250fd47cbcb50a7610d707714
sam.shen <sammffl@icloud.com>
2016-04-13 21:52:30 +0800

    java project

---
+++ src/com/sammffl/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sammffl;
+
+/**
+ * Created by SamMFFL on 2016/4/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("I love imooc!");
+    }
+}
