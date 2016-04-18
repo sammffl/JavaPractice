1846eb01d92d7742624d3f61d1f1663ba84f701c
sam.shen <sammffl@icloud.com>
2016-04-18 22:25:08 +0800

    +

--- src/com/sammffl/HelloWorld.java
+++ src/com/sammffl/HelloWorld.java
@@ -9,6 +9,28 @@ import java.util.*;
  * Created by SamMFFL on 2016/4/13.
  */
 public class HelloWorld {
+    private  String name = "imooc";
+    int age = 20;
+    private static String voi = "123";
+
+    public class Inner {
+        String name = "爱慕课";
+
+        public void show() {
+            System.out.println("外部类中的name" + HelloWorld.this.name);
+            System.out.println("内部类中的name：" + this.name);
+            System.out.println("外部类中的age:" + age);
+        }
+    }
+
+    public static class SInner {
+        String name = "爱慕课";
+
+        public void show() {
+            System.out.println("外部类中的name" + HelloWorld.voi);
+            System.out.println("内部类中的name：" + this.name);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("I love imooc!");
 
@@ -17,5 +39,10 @@ public class HelloWorld {
         System.out.println(telephone.getMem());
         int[] arr = {1, 2, 3, 4};
         Arrays.sort(arr);
+
+        HelloWorld o = new HelloWorld();
+        Inner inner = o.new Inner();
+        inner.show();
+
     }
 }
