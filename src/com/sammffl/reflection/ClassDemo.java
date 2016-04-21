a70bab09f90af566b611e5c8e3c79cf36d2bf293
SamMFFL <sammffl@icloud.com>
2016-04-21 15:14:56 +0800

    数据类型的类类型

---
+++ src/com/sammffl/reflection/ClassDemo.java
@@ -0,0 +1,24 @@
+package com.sammffl.reflection;
+
+/**
+ * Created by Administrator on 2016/4/21.
+ */
+public class ClassDemo {
+    public static void main(String[] args) {
+        Class c = int.class;
+        //class java.lang.String
+        Class c1 = String.class;
+        //double
+        Class c2 = double.class;
+        //class java.lang.Double
+        Class c3 = Double.class;
+        //void
+        Class c4 = void.class;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+
+    }
+}
