870e4b009cdef495a78a712379ce62d51d9124ef
SamMFFL <sammffl@icloud.com>
2016-04-21 15:17:55 +0800

    数据类型的类类型

--- src/com/sammffl/reflection/ClassUtil.java
+++ src/com/sammffl/reflection/ClassUtil.java
@@ -8,7 +8,8 @@ public class ClassUtil {
      * 打印类的信息，包括类的成员函数、成员变量
      * @param obj 该对象所属类的信息
      */
-    public static void printClassMessage(Object obj){
+    public static void printClassMessage(Object obj) {
         Class c = obj.getClass();
+        System.out.println("类的名称是：" + c.getName());
     }
 }
