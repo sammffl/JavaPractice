e5976b61930eefaef6e80b7f217881ce6f86a4df
sam.shen <sammffl@icloud.com>
2016-04-24 15:32:03 +0800

    annotation

---
+++ src/com/annotation/AnnTest.java
@@ -0,0 +1,18 @@
+package com.annotation;
+
+/**
+ * Created by SamMFFL on 2016/4/22.
+ */
+public class AnnTest {
+
+    @SuppressWarnings("deprecation")
+    public void sing() {
+        Person p = new Child();
+        p.sing();
+    }
+
+    @Description("func")
+    public String eyeColor() {
+        return "red";
+    }
+}
